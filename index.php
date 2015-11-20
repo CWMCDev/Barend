2719b81a82375e25ec640038093ce24aa03ef83c
chrisje <chriswitteveen2@hotmail.com>
2015-11-20 18:05:46 +0100

    creating wiki

--- index.php
+++ index.php
@@ -1,9 +1,33 @@
 <?php
 require 'vendor/autoload.php';
 
+// initialize slim app
 $app = new \Slim\Slim();
-$app->get('/hello/:name', function ($name) {
-    echo "Hello, " . $name;
+
+// define all routes
+$app->get('/portal', function () {
+    echo readfile('views/portal.html');
 });
+
+$app->get('/portal/students/grades', function () {
+    echo "Hello, ";
+});
+
+$app->get('/portal/students/presention', function () {
+    echo "Hello, ";
+});
+
+$app->get('/portal/students/profile', function () {
+    echo "Hello, ";
+});
+
+$app->get('/portal/students/class', function () {
+    echo "";
+});
+
+$app->get('/portal/students', function () {
+    echo "";
+});
+
 $app->run();
 ?>
