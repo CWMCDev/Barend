56d30028b81a15a4711a504657952ca568fef9a8
chrisje <chriswitteveen2@hotmail.com>
2015-11-22 20:25:06 +0100

    stuff

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ $app->get('/portal/students/grades', function () {
     }
     
     if($user == '' || $pass == '') {
-     $app->halt(401, 'Please set username and password first');
+     self::$app->halt(401, 'Please set username and password first');
     }
     
     $portal = new Portal();
