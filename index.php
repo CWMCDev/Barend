a3c7ecf057efa53909147c72a4d02d74d37f5859
Milo Cesar <milocesar1@gmail.com>
2015-11-27 13:11:59 +0100

    Removed superflous ')'

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ function createResponse($data=array()) {
 		array_walk_recursive($array, array ($xml, 'addChild'));
 		print $xml->asXML();
 	} else {
-		print json_encode($data, JSON_PRETTY_PRINT));
+		print json_encode($data, JSON_PRETTY_PRINT);
 	}
 }
 
