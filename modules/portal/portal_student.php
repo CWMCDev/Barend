2350f32ee38e9345c28f58a442887fdd2246220a
cwitteveen <chriswitteveen2@hotmail.com>
2015-12-02 13:50:30 +0100

    Update portal_student.php

--- modules/portal/portal_student.php
+++ modules/portal/portal_student.php
@@ -88,7 +88,15 @@ class Portal {
       $i = 0;
       $dag = 0;
       foreach($tr('td') as $uur) {
-      	$uren[$i] = array('uur'=>($i+1), 'status'=>str_replace("last-of-week","",$uur->class));
+      	
+      	$class = str_replace("last-of-week","",$uur->class);
+      	
+      	if ($class == 'melding-only ' || $class == 'geoorlafw') {
+      		$uren[$i] = array('uur'=>($i+1), 'status'=>$class, 'reden'=>$uur->getPlainText());
+      	} else {
+      		$uren[$i] = array('uur'=>($i+1), 'status'=>$class);
+      	}
+      	
         	
        	if ($i == 9) {
        		$dagen[$dagenNamen[$dag]] = $uren;
