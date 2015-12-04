842d3a8c15d0981695aa53130fd6400a08c5809d
Milo Cesar <milocesar1@gmail.com>
2015-12-04 16:57:29 +0100

    workaround for empty cache
    Workaround for loading grades

--- modules/portal/portal_student.php
+++ modules/portal/portal_student.php
@@ -76,6 +76,7 @@ class Portal {
  }
 
  public static function getGrades($periode) {
+  curl::get('https://leerlingen.candea.nl/Portaal/Cijferlijst/Examendossier/Cijferlijst?wis_ajax&ajax_object=727&periode727='.$periode, array(CURLOPT_COOKIE=>self::$cookiestr, CURLOPT_FOLLOWLOCATION=>1, CURLOPT_SSL_VERIFYPEER=>false, CURLOPT_TIMEOUT=>6));
   return array(
    'periode'=>$periode,
    'classes'=>self::parseGrades(curl::get('https://leerlingen.candea.nl/Portaal/Cijferlijst/Examendossier/Cijferlijst?wis_ajax&ajax_object=727&periode727='.$periode, array(CURLOPT_COOKIE=>self::$cookiestr, CURLOPT_FOLLOWLOCATION=>1, CURLOPT_SSL_VERIFYPEER=>false, CURLOPT_TIMEOUT=>6)))
