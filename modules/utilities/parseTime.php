33af82360c2de9e195f1197ea586f96c8fc7f028
cwitteveen <chriswitteveen2@hotmail.com>
2015-12-08 12:23:18 +0100

    Update parseTime.php

--- modules/utilities/parseTime.php
+++ modules/utilities/parseTime.php
@@ -1,7 +1,9 @@
 <?php
-  function getTime() {
+class parseTime {
+   function getTime() {
     $week = date('W', 1448869500);
     $day = date('l', 1448869500);
     echo $week . ' : ' . $day;
-  }
+  } 
+}
 ?>
