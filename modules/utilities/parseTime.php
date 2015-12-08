55730929b788b60516d47726caa43ae3dc8b6b27
chrisje <chriswitteveen2@hotmail.com>
2015-12-08 17:10:06 +0100

    Day now in dutch

--- modules/utilities/parseTime.php
+++ modules/utilities/parseTime.php
@@ -1,9 +1,12 @@
 <?php
 class parseTime {
    static function getTime() {
-    $week = date('W', 1448869500);
     $day = date('l', 1448869500);
-    echo $week . ' : ' . $day;
+    switch($day){
+      case "Monday": 
+        echo 'Maandag';
+        break;
+    }
   } 
 }
 ?>
