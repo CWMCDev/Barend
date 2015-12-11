65152edaff124d99068acac9f7ff00c16274623e
Milo Cesar <milo526@users.noreply.github.com>
2015-12-11 13:53:31 +0100

    Added page index

--- modules/portal/portal_student.php
+++ modules/portal/portal_student.php
@@ -164,9 +164,10 @@ public static function getClassList() {
   echo $pageCount = ceil($maxStudentCount / $studentPerPage);
   
   $classList = array();
-  for ($x = 1; $x <= $pageCount; $x++) {
-    curl::get('https://leerlingen.candea.nl/Portaal/Presentie/Presentie?wis_ajax&ajax_object=724', array(CURLOPT_COOKIE=>self::$cookiestr, CURLOPT_FOLLOWLOCATION=>1, CURLOPT_SSL_VERIFYPEER=>false, CURLOPT_TIMEOUT=>6));
-    $currentClassList = self::parseClassList(curl::get('https://leerlingen.candea.nl/Portaal/Presentie/Presentie?wis_ajax&ajax_object=724', array(CURLOPT_COOKIE=>self::$cookiestr, CURLOPT_FOLLOWLOCATION=>1, CURLOPT_SSL_VERIFYPEER=>false, CURLOPT_TIMEOUT=>6)));
+  for ($x = 0; $x < $pageCount; $x++) {
+    $pageStart = $x*8 + 1;
+    curl::get('https://leerlingen.candea.nl/Portaal/Presentie/Presentie?wis_ajax&ajax_object=724&start724={$pageStart}', array(CURLOPT_COOKIE=>self::$cookiestr, CURLOPT_FOLLOWLOCATION=>1, CURLOPT_SSL_VERIFYPEER=>false, CURLOPT_TIMEOUT=>6));
+    $currentClassList = self::parseClassList(curl::get('https://leerlingen.candea.nl/Portaal/Presentie/Presentie?wis_ajax&ajax_object=724&start724={$pageStart}', array(CURLOPT_COOKIE=>self::$cookiestr, CURLOPT_FOLLOWLOCATION=>1, CURLOPT_SSL_VERIFYPEER=>false, CURLOPT_TIMEOUT=>6)));
     $classList = array_merge($classList, $currentClassList);
   } 
   
