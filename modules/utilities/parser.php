407e0939d4bff1d8a4d0db9f014df085c9d4045a
Milo Cesar <milo526@users.noreply.github.com>
2015-12-11 14:19:22 +0100

    Renamed parser
    added new parsing

--- modules/utilities/parser.php
+++ modules/utilities/parser.php
@@ -21,4 +21,11 @@ class parseTime {
     }
   } 
 }
+
+function replace_unicode_escape_sequence($match) {
+    return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UCS-2BE');
+}
+function unicode_decode($str) {
+    return preg_replace_callback('/\\\\u([0-9a-f]{4})/i', 'replace_unicode_escape_sequence', $str);
+}
 ?>
