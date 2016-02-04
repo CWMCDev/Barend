fb785b6d36aeef0d28ab962313e4c767f0aac7bf
chrisje <chriswitteveen2@hotmail.com>
2016-02-05 00:44:57 +0100

    Fixed one problem, created another

--- index.php
+++ index.php
@@ -173,6 +173,10 @@ $app->get('/zportal/schedule/:type/:id/:week/:token/:user/:userToken', function(
     }
     usort($scheduleData, "cmp");
     
+    createResponse($scheduleData);
+    
+    echo '<br><br>'
+    
     $authStatus = checkAuth($user, $userToken);
     if($authStatus === true){
       $password = getPassword($user, $userToken);
